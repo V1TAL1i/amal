6a5066161f3b00b4ddc27941b227363121cbc954
V1TAL1i <120783926+V1TAL1i@users.noreply.github.com>
2022-12-24 19:03:59 +0600

    icons

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import "./index.css";
 import logo from "./img/logo.webp";
-
+import image from "./img/buckery.jpg";
+import {IoIosSearch} from "react-icons/io";
+import {CiHeart} from "react-icons/ci";
+import {CgShoppingBag} from "react-icons/cg";
+ 
 export default function Header() {
   return (
     <div className="Header">
@@ -15,25 +19,43 @@ export default function Header() {
           </div>
         </div>
       </div>
-      
+
       <div className="Header_nav">
         <div className="container">
-      <div className="Bucker">
-        <img src={logo} alt="" />
-
-      </div>
-      <div className="nav">
-        <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Blog</li>
-            <li>Shop</li>
-            <li>Contact</li>
-        </ul>
+          <div className="Bucker">
+            <img src={logo} alt="" />
+          </div>
+          <div className="nav">
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Blog</li>
+              <li>Shop</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+          <div className="icons">
+            <IoIosSearch/>
+            <CiHeart/>
+            <CgShoppingBag/>
+          </div>
         </div>
-      <div></div>
+
+        {/* <div className="Header_average">
+          <div className="container2">
+            <div className="buckery_nav1">
+              <img src={image} className="image" alt="" />
+            </div>
+
+            <div className="text">
+              <h1>About Us</h1>
+              <ul>
+                <li>Home</li>
+              </ul>
+            </div>
+          </div>
+        </div> */}
       </div>
-    </div>
     </div>
   );
 }
