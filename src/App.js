e14096fb29d42bb332b23656d6254505ab6330cc
V1TAL1i <120783926+V1TAL1i@users.noreply.github.com>
2023-02-07 19:33:43 +0600

    konec

--- src/App.js
+++ src/App.js
@@ -20,6 +20,8 @@ import services4 from "./../src/img/services4.webp";
 import shipping1 from "./img/shipping1.webp";
 import shipping2 from "./img/shipping2.webp";
 import shipping3 from "./img/shipping3.webp";
+import strelka from "./img/4723461.png";
+import paypel from "./img/paypal.webp";
 
 
 function Header() {
@@ -170,12 +172,94 @@ function App() {
 
         </div>
         <div className="infa">
+<div className="contacts">
+  <div className="conta">
+    <h2>CONTACT US</h2></div>
+  <div className="you">
+<p>If you have any question.please contact us at <a href="#" className="email">demo@example.com</a> </p>
+</div>
+<div className="your">
+<p>Your address goes here. 123,
+   Address.</p>
+</div>
+<div className="number">
+<a href="">+00 123 456 789</a>
+<a href="">+00 123 456 789</a>
+</div>
+
+</div>
+
+
+<div className="celoe">
+
+<div className="infar"> 
+<div className="formation">
+<div className="infor">
+<h2>INFORMATION </h2>
+</div>
+<div className="mation">
+<a href="" >About us</a >
+<a href="" >Delivery information</a>
+<a href="" >Privacy Policy</a >
+<a href="" >Sales</a >
+<a href="" >Terms & Conditions</a >
+<a href="" >Shipping Policy</a >
+<a href="" >EMI Payment</a>
+</div>
+</div>
+<div className="account">
+<div className="acc">
+<h2>ACCOUNT</h2>
+</div>
+<div className="count">
+<a href="" >My account</a>
+<a href="" >My orders</a>
+<a href="" >Returns</a>
+<a href="" >Shipping</a>
+<a href="" >Wishlist</a>
+<a href="" >How Does It Work</a>
+<a href="" >Merchant Sign Up</a>
+</div>
+</div>
+
+
+
+
+</div>
+
+
+
+</div>
+<div className="newsletter"> 
+<div className="news">
+
+<h2>NEWSLETTER</h2>
+</div>
+<div className="have">
+  <p>If you have any question.please contact us at <a href="" className="send">Send Us a Email</a></p>
+</div>
+<div className="adress">
+  <h4 className="adre">Email Address</h4>
+  <div className="strelka">
+<img src={strelka} alt="" className="rek" />
+  </div>
+  <div className="paypel">
+    <img src={paypel} alt="" className="payp"/>
+  </div>
+  
+</div>
 
         </div>
 
 
+        <div className="konec">
+  <p>© 2022 Bucker. Made With  By CodeCarnival</p>
+</div>
 
+        </div>
+        
       </div>
+      
     </div>
 
   );
